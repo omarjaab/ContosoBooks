cfe660b5d403bb221a6080d2749a6e99e25da594
omarjaab <omarjaab@outlook.com>
2016-05-18 21:20:55 +0200

    third commit

--- src/MvcMovie/Migrations/20160517132000_Initial.Designer.cs
+++ src/MvcMovie/Migrations/20160517132000_Initial.Designer.cs
@@ -8,9 +8,10 @@ using MvcMovie.Models;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160517132000_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
